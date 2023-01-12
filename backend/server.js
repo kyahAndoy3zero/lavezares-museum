b1096d1481a5c251fdd2f49d9c0af4aa19fada95
Zandro Gene Ortecio <ortecio.zandrogene30@gmail.com>
2023-01-12 21:24:24 +0800

    importing cors

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 8000;
 const app = express();
-
+const cors = require('cors')
 
 app.use(express.json());
 app.use(cors())
