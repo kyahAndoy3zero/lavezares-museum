d46bbb8dcee15ecc2a8ae44f130cc2a72e2295e9
Zandro Gene Ortecio <ortecio.zandrogene30@gmail.com>
2023-01-12 22:53:50 +0800

    vercel

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const dotenv = require('dotenv').config()
-const port = process.env.PORT || 8000;
+
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 
 app.use(express.json());
 app.use(cors())
 
 app.use('/api/v1/reservations', require('./routes/reservationRoutes'))
 
+
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
