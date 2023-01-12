3cc74867830391162c456629ab3b5acc46f4a85d
Zandro Gene Ortecio <ortecio.zandrogene30@gmail.com>
2023-01-12 23:43:17 +0800

    Service was fixed

--- frontend/src/components/ServiceSubmit/Service.js
+++ frontend/src/components/ServiceSubmit/Service.js
@@ -1,13 +1,13 @@
 import axios from "axios"
 
 
-const REMOTE_URL = "https://lavezares-museum.vercel.app"
+const REMOTE_URL = "https://node-api-53f4.onrender.com"
 const API_URL = `${REMOTE_URL}/api/v1/reservations`
 
 
 const createInquiry = async (personData) => {
     try {
-        const response = await axios.post(URL + API_URL, personData);
+        const response = await axios.post(API_URL, personData);
         return response.data;
     } catch (error) {
         console.error(error);
