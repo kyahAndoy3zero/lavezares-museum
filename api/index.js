7e5c4957810a988b31979bb3060d759fc50c6ade
Zandro Gene Ortecio <ortecio.zandrogene30@gmail.com>
2023-01-12 23:15:39 +0800

    move index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors())
 
-app.use('/api/v1/reservations', require('./routes/reservationRoutes'))
+app.use('/api/v1/reservations', require('../backend/routes/reservationRoutes'))
 
 
 
