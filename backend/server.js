9332f8d1c97211c62cc6bd7ba0a4bf722004868d
Zandro Gene Ortecio <ortecio.zandrogene30@gmail.com>
2023-01-12 21:17:31 +0800

    cors added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const app = express();
 
 
 app.use(express.json());
+app.use(cors())
 
 app.use('/api/v1/reservations', require('./routes/reservationRoutes'))
 
--- frontend/src/components/ServiceSubmit/Service.js
+++ frontend/src/components/ServiceSubmit/Service.js
@@ -2,10 +2,11 @@ import axios from "axios"
 
 
 const API_URL = "/api/v1/reservations"
+const URL = "https://lavezares-museum.vercel.app"
 
 const createInquiry = async (personData) => {
     try {
-        const response = await axios.post(API_URL, personData);
+        const response = await axios.post(URL + API_URL, personData);
         return response.data;
     } catch (error) {
         console.error(error);
